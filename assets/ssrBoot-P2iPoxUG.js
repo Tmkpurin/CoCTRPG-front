5a02cb81c9003bc39ca8e5b81e00ff200be30e3e
Tmkpurin <cecillia10827@gmail.com>
2025-03-04 08:36:17 +0000

    Deploying to gh-pages from @ Tmkpurin/CoCTRPG-front@7065d313171bee6c85e380a8ad5582a4fde0d487 🚀

--- assets/ssrBoot-P2iPoxUG.js
+++
@@ -1,1 +0,0 @@
-import{s,A as t,o as e,aX as n}from"./index-Da_Xni5f.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
